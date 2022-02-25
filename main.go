8564f4cc53b58bc5ee859160f3277984f8f3154e
Thilina Lakshan <thilinalakshanmail@gmail.com>
2022-02-26 04:25:03 +0530

    version 2

--- main.go
+++ main.go
@@ -2,69 +2,18 @@ package main
 
 import (
 	"fmt"
-	"github.com/AlecAivazis/survey/v2"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"regexp"
-	"strconv"
+	"packer/config"
 	"strings"
 )
 
-type PathInfo struct {
-    Name    string
-    Size    int64
-    IsDir   bool
-    Path   string
-}
-
-func calcSize(info os.FileInfo) int64 {
-    // if directory calculate size of all files in it 
-    if info.IsDir() {
-        var size int64
-        filepath.Walk(info.Name(), func(_ string, info os.FileInfo, err error) error {
-            if err != nil {
-                return err
-            }
-            if !info.IsDir() {
-                size += info.Size()
-            }
-            return nil
-        })
-        return size
-    }
-    return info.Size()
-}
-
-func getPathInfoList(paths []string) ([]PathInfo, error) {
-    var files []PathInfo
-    for _, path := range paths {
-        info, err := os.Stat(path)
-        if err != nil {
-            return nil, err
-        }
-        files = append(files, PathInfo{
-            Name:    info.Name(),
-            Size:    calcSize(info),
-            IsDir:   info.IsDir(),
-
-        })
-
-    }
-    return files, nil
-}
-
-func tarGzGpg(args *PackerArgs) error {
-    paths := make([]string, len(args.files))
-    for i, file := range args.files {
-        paths[i] = file.Path
-    }
-    
+func tarGzGpg(args *config.PackerArgs) error {
 	parts := []string{
-		fmt.Sprintf("tar -cf - %s", strings.Join(paths, " ")),
-		fmt.Sprintf("pv -s %v", args.FileSize()),
+		fmt.Sprintf("tar -cf - %s", strings.Join(args.GetSelectedFilesRawPaths(), " ")),
+		fmt.Sprintf("pv -s %v", args.GetSelectedFileSize()),
 		"gzip",
-		fmt.Sprintf("gpg --passphrase %s --batch --quiet --yes -c > '%s'", args.paraphrase, args.output),
+		fmt.Sprintf("gpg --passphrase %s --batch --quiet --yes -c > '%s'", args.Paraphrase, args.Output),
 	}
 	cmd := exec.Command("sh", "-c", strings.Join(parts, " | "))
 	cmd.Stdout = os.Stdout
@@ -77,116 +26,14 @@ func tarGzGpg(args *PackerArgs) error {
 	return nil
 }
 
-type PackerArgs struct {
-	files      []PathInfo
-	paraphrase string
-	output     string
-}
-
-func (args *PackerArgs) FileSize() int64 {
-    var size int64
-    for _, file := range args.files {
-        size += file.Size
-    }
-    return size
-}
-
-func NewPackerArgs() (*PackerArgs, error) {
-	var args PackerArgs
-	// set paraphrase
-	args.paraphrase = os.Getenv("PACKER_PASS")
-	if args.paraphrase == "" {
-		fmt.Println("PACKER_PASS is not set")
-		err := survey.AskOne(&survey.Password{Message: "Paraphrase:"}, &args.paraphrase)
-		if err != nil {
-			return nil, err
-		}
-		if args.paraphrase == "" {
-			return nil, fmt.Errorf("Paraphrase is empty")
-		}
-	} else {
-		fmt.Println("Load paraphrase from env variable PACKER_PASS")
-	}
-
-	// set output file path
-	if len(os.Args) >= 3 {
-		args.output = os.Args[2]
-	} else {
-		args.output = os.Getenv("PACKER_OUT")
-	}
-	if args.output == "" {
-		fmt.Println("No valid output file specified")
-		err := survey.AskOne(&survey.Input{Message: "Output file:"}, &args.output)
-		if err != nil {
-			return nil, err
-		}
-		if args.output == "" {
-			return nil, fmt.Errorf("output file is empty")
-		}
-		if !strings.HasSuffix(args.output, ".tar.gz.gpg") {
-			args.output += ".tar.gz.gpg"
-			fmt.Printf("output file is set to %s\n", args.output)
-		}
-	} else {
-		if len(os.Args) >= 3 {
-			fmt.Printf("Load output file from arguments\n")
-		} else {
-			fmt.Printf("Load output_file='%s' from env PACKER_OUT\n", args.output)
-		}
-	}
-
-	path := os.Args[1]
-    files, err := getPathInfoList([]string{path})
-	if err != nil {
-		return nil, err
-	}
-
-
-	var items []string
-	for i, file := range files {
-		// skip hidden files
-		if file.Name[0] == '.' {
-			continue
-		}
-		items = append(items, fmt.Sprintf("(%v) [%v MB] %s", i+1, file.Size/1024/1024, file.Name))
-	}
-
-	var selected []string
-	err = survey.AskOne(&survey.MultiSelect{
-		Message:  "Select files:",
-		Options:  items,
-		PageSize: 20,
-	}, &selected)
-	if err != nil {
-		return nil, err
-	}
-	indexRegex := regexp.MustCompile(`^\((\d+)\)`)
-	for _, item := range selected {
-		position, _ := strconv.Atoi(indexRegex.FindStringSubmatch(item)[1]) 
-        args.files = append(args.files, files[position-1])
-	}
-	if len(args.files) == 0 {
-		return nil, fmt.Errorf("no files selected")
-	}
-	return &args, nil
-}
-
 func main() {
-	// exit if os args are not enough
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: packer <path-to-check> [output-file]")
-		return
-	}
-
-	args, err := NewPackerArgs()
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return
-	}
-
-	err = tarGzGpg(args)
+	args, err := config.New()
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = tarGzGpg(args)
+	if err != nil {
 		return
 	}
 }
